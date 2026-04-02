f9347d8eceb165e18883d1833149e6103ea48753
iyonicorp <iyonicorp@gmail.com>
2026-04-03 00:12:56 +0300

    L

--- server.js
+++ server.js
@@ -88,6 +88,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on ${PORT}`);
 });
